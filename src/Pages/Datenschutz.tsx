3b429b16dbc37f6228d0228fcbda6fb8f2f816fa
jayzone91 <register@j4yz0n3.de>
2024-10-17 14:06:20 +0200

    Update Datenschutz.tsx

--- src/Pages/Datenschutz.tsx
+++ src/Pages/Datenschutz.tsx
@@ -13,7 +13,8 @@ export default function Datenschutz() {
       <MainLayout title="Datenschutz&shy;erklärung" subtitle="">
         <div className="flex items-center w-full my-5">
           <NavLink
-            to="/Auftragsverarbeitung"
+            to="https://dsgvo3.ds-manager.net/computerextra/onlinevt.html?key=6ThOZSR1cvQCMdES"
+            target="_blank"
             className="relative px-10 py-5 overflow-hidden font-medium text-gray-600 bg-gray-100 border border-gray-100 rounded-lg shadow-inner group"
           >
             <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
