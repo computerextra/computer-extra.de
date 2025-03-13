d9fecd324f5ebe0a5719c7260fb0a693e060e077
jayzone91 <register@j4yz0n3.de>
2025-03-13 09:57:36 +0100

    Update mail.php

--- public/php/mail.php
+++ public/php/mail.php
@@ -64,21 +64,7 @@ if (isset($lebenslauf)) {
 
 // Check if IP is in Database
 $ip = $_SERVER['REMOTE_ADDR'];
-// Connect to Database
-$mysqli = new mysqli(MYSQL_SERVER, MYSQL_USER_FILTER, MYSQL_PASS_FILTER, MYSQL_DB_FILTER);
-if ($mysqli->connect_errno) {
-    echo (json_encode(["success" => false]));
-    die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
-}
-$sql = "SELECT * FROM IP WHERE ip=?";
-$statement = $mysqli->prepare($sql);
-$statement->bind_param('s', $ip);
-$statement->execute();
-$result = $statement->get_result();
-if ($result->num_rows > 0) {
-    echo (json_encode(["success" => false]));
-    die();
-}
+
 
 $anschreiben = false;
 $lebenslauf  = false;
@@ -116,7 +102,7 @@ try {
         $mail->addAttachment('uploads/Lebenslauf.pdf');
     }
 
-    $body = 'Email von: ' . $data["Name"] . '<br>Mail: ' . $data["Mail"] . '<br>Telefon: ' . $data["Phone"] . '<br>Bewerbung als: ' . $data["Job"];
+    $body = 'IP: ' . $ip . '<br><br>Email von: ' . $data["Name"] . '<br>Mail: ' . $data["Mail"] . '<br>Telefon: ' . $data["Phone"] . '<br>Bewerbung als: ' . $data["Job"];
     if ($data["Job"] === "Ausbildung") {
         $body .= '<br>Ausbildung: ' . $data["Ausbildung"];
     }
