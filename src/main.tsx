44c1e8bf543bba388d797bf07fef7953541c2052
jayzone91 <register@j4yz0n3.de>
2025-08-26 13:05:18 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -13,15 +13,19 @@ createRoot(document.getElementById("root")!).render(
         <Route element={<Layout />}>
           <Route index element={<Startseite />} />
           <Route path="Angebote" element={<AngeboteSeite />} />
-          <Route path="Leistungen">
-            <Route index element={<>Leistungen</>} />
-          </Route>
+          {/* Leistungen */}
+          <Route
+            path="Webentwicklung"
+            element={<div>Webentwicklung Page</div>}
+          />
           <Route path="Partner" element={<div>Partner Page</div>} />
           <Route path="Team" element={<div>Team Page</div>} />
           <Route path="Jobs" element={<div>Jobs Page</div>} />
           <Route path="Fernwartung" element={<div>Fernwartung Page</div>} />
+          {/* Termin */}
           <Route path="Termin" element={<div>Termin Page</div>} />
           <Route path="Kontakt" element={<div>Kontakt Page</div>} />
+          {/* Gesetzliches */}
           <Route path="Impressum" element={<div>Impressum Page</div>} />
           <Route path="Datenschutz" element={<div>Datenschutz Page</div>} />
           <Route path="AGB" element={<div>AGB Page</div>} />
