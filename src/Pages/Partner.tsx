ce719a08a1ae1756ddc68c74326e2b5661079178
jayzone91 <register@j4yz0n3.de>
2025-08-27 13:46:07 +0200

    Update Partner.tsx

--- src/Pages/Partner.tsx
+++ src/Pages/Partner.tsx
@@ -35,22 +35,34 @@ function PartnerCard({ Partner, id }: { Partner: Partner; id: number }) {
     <>
       <Card
         className={cn(
-          isLarge() ? "lg:col-span-3 flex-row" : "col-span-1 flex-col",
-          "flex "
+          isLarge() ? "lg:col-span-3 lg:flex-row" : "col-span-1",
+          "flex flex-col"
         )}
       >
         <img
           // TODO: Link anpassen, wenn direkt auf API
           src={"https://computer-extra.de/Images/Partner/" + Partner.image}
           alt={Partner.name}
-          className={cn("object-contain w-full", isLarge() ? "h-70" : "h-48")}
+          className={cn(
+            "object-contain w-full",
+            isLarge() ? "lg:h-70 h-48" : "h-48"
+          )}
         />
         <div className="flex flex-col justify-around flex-1">
           <CardHeader className="text-xl font-bold text-center">
             {Partner.name}
           </CardHeader>
           <CardContent className="flex justify-center">
-            <Button asChild>
+            <Button className="lg:hidden" asChild size={"default"}>
+              <a href={Partner.link} target="_blank" rel="noopener noreferrer">
+                Webseite besuchen
+              </a>
+            </Button>
+            <Button
+              className="hidden lg:inline-flex"
+              asChild
+              size={isLarge() ? "lg" : "default"}
+            >
               <a href={Partner.link} target="_blank" rel="noopener noreferrer">
                 Webseite besuchen
               </a>
