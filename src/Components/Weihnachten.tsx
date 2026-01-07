c645338270b82bb2aae236c455311402f00e6a35
computerextra <register@j4yz0n3.de>
2026-01-07 09:26:05 +0100

    Weihnachtslayout wieder raus

---
+++ src/Components/Weihnachten.tsx
@@ -0,0 +1,10 @@
+export default function Weihnachten() {
+  return (
+    <h2 className="text-xl lg:text-2xl font-semibold text-slate-100 mt-3 md:mt-[3rem] w-full lg:w-[50%] max-w-fit rounded-xl px-10 md:py-4 bg-red-800/70 border-blue-900/70">
+      Sehr geehrte Kund:innen, <br />
+      unser Geschäft ist am 24.12.2025, 31.12.2025 sowie am 02.01.2026
+      geschlossen. <br />
+      Ab dem 05.01.2026 sind wir wieder wie gewohnt für Sie da.
+    </h2>
+  );
+}
