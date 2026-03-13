1ac6d9edb7df4379e8609265441511b970de58b8
computerextra <register@j4yz0n3.de>
2026-03-13 15:31:33 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const PCVisitLink =
-  "https://lb3.pcvisit.de/v1/hosted/jumplink?func=download&topic=guestSetup&destname=pcvisit_Kunden-Modul";
+  "https://stable-update.pcvisit.de/v1/hosted/jumplink?func=download&topic=guestSetup&productrole=guestSetup&gateway=stable-update.pcvisit.de&companyid=4500788308";
 
 export const DL_VALUE = 30;
