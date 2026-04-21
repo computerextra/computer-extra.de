db7aa607feefdfbbc69daeaa742408dba528fb82
Johannes Kirchner <register@j4yz0n3.de>
2026-04-21 10:56:43 +0200

    Erfolg / Fehler

--- src/Pages/Erfolg.tsx
+++ src/Pages/Erfolg.tsx
@@ -1,7 +1,11 @@
 const Erfolg = () => {
   return (
-    <div>
+    <div className="container mx-auto mt-5">
       <title>Computer Extra GmbH | Erfolg</title>
+      <div className="text-center text-5xl font-bold text-green-700">
+        Wir haben Ihre Nachricht erhalten und werden uns mit Ihnen in Verbindung
+        setzen.
+      </div>
     </div>
   )
 }
--- src/Pages/Fehler.tsx
+++ src/Pages/Fehler.tsx
@@ -1,7 +1,14 @@
 const Fehler = () => {
   return (
-    <div>
+    <div className="container mx-auto mt-5">
       <title>Computer Extra GmbH | Fehler</title>
+      <div className="text-center text-5xl font-bold text-red-700">
+        Leider konnte Ihre Mail nicht verschickt werden. <br />
+        Bitte prüfen Sie Ihre Eingaben und versuchen Sie es später noch einmal.{" "}
+        <br />
+        Sie können uns auch innerhalb unserer Öffnungszeiten telefonisch
+        erreichen.
+      </div>
     </div>
   )
 }
