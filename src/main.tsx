b3757f78f07dae786bfacb101852007390fe87b9
Johannes Kirchner <register@j4yz0n3.de>
2026-04-21 11:11:59 +0200

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import posthog from "posthog-js"
 import { PostHogProvider } from "posthog-js/react"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { BrowserRouter } from "react-router"
 import App from "./App.tsx"
 import "./index.css"
 
 const queryClient = new QueryClient()
 
-// TODO: PostHog was initialized without a token. This likely indicates a misconfiguration. Please check the first argument passed to posthog.init()
 posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
   defaults: "2025-05-24",
